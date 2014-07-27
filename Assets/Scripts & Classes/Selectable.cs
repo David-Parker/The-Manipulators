479047949bfad584cb47a1adf7feb545c72005fe
John McVeigh <mcveigh.john.p@gmail.com>
2014-07-27 17:54:50 -0400

    Basic selector. (Multi/boxing/indicator soon)

---
+++ Assets/Scripts & Classes/Selectable.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Selectable : MonoBehaviour {
+
+	public bool isSelected;
+
+	private UnitManager unitManager;
+
+	
+	void Awake()
+	{
+		isSelected = false;
+		unitManager = Camera.main.GetComponentInChildren<UnitManager>();
+	}
+
+	void Update()
+	{
+		if(isSelected)
+		{
+
+		}
+	}
+
+	void Selected()
+	{
+		// UnitManager select this object
+		unitManager.SelectSingleUnit(gameObject);
+		Debug.Log("Selected: " + gameObject);
+
+		isSelected = true;
+	}
+}
---
+++ Assets/Scripts & Classes/UnitManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UnitManager : MonoBehaviour 
+{
+	public List<GameObject> selectedUnits;
+
+	void Start()
+	{
+		selectedUnits.Clear();
+	}
+
+	public void SelectSingleUnit(GameObject unit)
+	{ 
+		foreach(GameObject obj in selectedUnits)
+		{
+			obj.GetComponent<Selectable>().isSelected = false;
+		}
+		selectedUnits.Clear();
+
+		selectedUnits.Add(unit);
+	}
+}
