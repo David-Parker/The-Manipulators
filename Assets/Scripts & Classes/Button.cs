32326409c3a5dc218ee4f3bc83bfc7c8c97b130c
David Parker <davidgparker123@gmail.com>
2014-07-29 21:11:30 -0500

    Started button class

---
+++ Assets/Scripts & Classes/Button.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+	public void buttonAction() {
+		/* Add Button Functionality here. For example   */
+		/* loading a new level or scene, opening a menu */
+		/* dialog or performing an in-game action.      */
+		Debug.Log("Button Pressed");
+	}
+}
---
+++ Assets/Scripts & Classes/ButtonSelector.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonSelector : MonoBehaviour {
+
+	private Camera cam;
+
+	void Start () {
+		cam = Camera.main;
+	}
+	
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			 if(Physics.Raycast(ray, out hit)) {
+			 	Button button = hit.collider.gameObject.GetComponent<Button>();
+			 	if(hit.collider != null && button != null) {
+			 		button.buttonAction();
+			 	}
+			 }
+		}
+	}
+}
