c56cf17351ffd9ae75d78735cc35906f42392465
Sutharshan Ram <sutharshanram@Sutharshans-iMac.local>
2014-08-06 17:48:58 +0530

    KeyboardInput script added. Has the basic hide/show cursor

---
+++ Assets/Scripts & Classes/KeyboardInput.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardInput : MonoBehaviour {
+
+	public bool showCursor = true; // global variable to disable cursor
+	public bool keyInterruptAllowed = true; // global variable to disable keys
+
+	void Start (){
+
+		Screen.showCursor = showCursor;
+	}
+
+	void Update () {
+
+		if(keyInterruptAllowed)
+		{
+		if(Input.GetKeyDown(KeyCode.X)){
+		   if(Screen.showCursor){
+				Debug.Log("Cursor Off");
+		   		Screen.showCursor = false;
+			}
+		   else if(showCursor){
+				Debug.Log("Cursor On");
+				Screen.showCursor = true;
+			}
+		}
+		}
+	}
+}
